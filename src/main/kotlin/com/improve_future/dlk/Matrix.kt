0cbf0093eeba8d82c184198ee758944f56f71dd1
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-09-26 14:11:43 +0900

    add div operation

--- src/main/kotlin/com/improve_future/dlk/Matrix.kt
+++ src/main/kotlin/com/improve_future/dlk/Matrix.kt
@@ -1,6 +1,30 @@
 package com.improve_future.dlk
 
-class Matrix(val rowSize: Int, val colSize: Int) {
+class Matrix(val rowSize: Int, val colSize: Int): Iterable<Pair<Int, Int>> {
+    override fun iterator(): Iterator<Pair<Int, Int>> {
+        return object: Iterator<Pair<Int, Int>> {
+            var rowIndex: Int = 0
+            var colIndex: Int = 0
+
+            override fun hasNext(): Boolean {
+                return rowIndex < rowSize - 1 || colIndex < colSize - 1
+            }
+
+            override fun next(): Pair<Int, Int> {
+                colIndex += 1
+                if (colIndex == colSize) {
+                    colIndex = 0
+                    rowIndex += 1
+                }
+                return Pair(rowIndex, colIndex)
+            }
+        }
+    }
+
+    inline fun forEach(block: (Double) -> Unit) {
+        for ((first, second) in this) block(this[first, second])
+    }
+
     val values: Array<DoubleArray>
 
     constructor(values: Array<DoubleArray>): this(values.size, values[0].size) {
@@ -77,7 +101,7 @@ class Matrix(val rowSize: Int, val colSize: Int) {
             if (rowIndex != 0) str += "\n"
             row.forEachIndexed { colIndex, value ->
                 if (colIndex != 0) str += " "
-                str += value.toString()
+                str += String.format("%.${decimalPoint}f", value)
             }
         }
         return str
@@ -112,8 +136,32 @@ class Matrix(val rowSize: Int, val colSize: Int) {
         }
         return matrix
     }
+
+    operator fun div(other: Number): Matrix {
+        return div(other.toDouble())
+    }
+
+    operator fun div(other: Double): Matrix {
+        val matrix = Matrix(this.rowSize, this.colSize)
+        this.values.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, value ->
+                matrix[rowIndex, colIndex] = value / other
+            }
+        }
+        return matrix
+    }
 }
 
 operator infix fun Number.times(other: Matrix): Matrix {
     return other.times(this)
+}
+
+operator infix fun Number.div(other: Matrix): Matrix {
+    val matrix = Matrix(other.rowSize, other.colSize)
+    other.values.forEachIndexed { rowIndex, row ->
+        row.forEachIndexed { colIndex, value ->
+            matrix[rowIndex, colIndex] = this.toDouble() / value
+        }
+    }
+    return matrix
 }
