f392c42c2f2d1a889249b4b454c629bf644c82c5
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-09-27 15:05:19 +0900

    add activation function tests

--- src/test/kotlin/com/improve_future/dlk/ActivationFunctionTest.kt
+++ src/test/kotlin/com/improve_future/dlk/ActivationFunctionTest.kt
@@ -1,7 +1,32 @@
 package com.improve_future.dlk
 
-/**
- * Created by kenji on 17/09/27.
- */
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class ActivationFunctionTest {
+    @Test
+    fun testStepFunction() {
+        val a = Matrix(arrayOf(arrayOf(0.0, 0.1)))
+        val b = ActivationFunction.stepFunction(a)
+        assertEquals(0.0, b[0, 0])
+        assertEquals(1.0, b[0, 1])
+    }
+
+    @Test
+    fun testRelu() {
+        val a = Matrix(arrayOf(arrayOf(0.0, 0.1, -0.1)))
+        val b = ActivationFunction.stepFunction(a)
+        assertEquals(0.0, b[0, 0])
+        assertEquals(1.0, b[0, 1])
+        assertEquals(0.0, b[0, 2])
+    }
+
+    @Test
+    fun testSigmoidFunction() {
+        val a = Matrix(arrayOf(arrayOf(-1.0, 0.0, 1.0)))
+        val b = ActivationFunction.sigmoid(a)
+        assertEquals(1.0 / (1 + Math.exp(1.0)), b[0, 0])
+        assertEquals(1.0 / (1 + Math.exp(0.0)), b[0, 1])
+        assertEquals(1.0 / (1 + Math.exp(-1.0)), b[0, 2])
+    }
 }
