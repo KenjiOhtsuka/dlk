4d4f7bb713862bbb7fce9d14049688fc037a201a
Kenji Ohtsuka <kok.fdcm@gmail.com>
2017-09-28 21:13:52 +0900

    add gradient function

--- src/main/kotlin/com/improve_future/dlk/GradientFunction.kt
+++ src/main/kotlin/com/improve_future/dlk/GradientFunction.kt
@@ -4,14 +4,18 @@ object GradientFunction {
     val h = 1e-4
 
     fun numericalDiff(f: (Matrix) -> Matrix, x: Matrix): Matrix {
-        return (f(x + h) - f(x)) / (2 * h)
+        return (f(x + h) - f(x - h)) / (2 * h)
     }
 
     fun numericalDiff(f: (Double) -> Double, x: Double): Double {
-        return (f(x + h) - f(x)) / (2 * h)
+        return (f(x + h) - f(x - h)) / (2 * h)
     }
 
     fun numericalDiff(f: (Double) -> Double, x: Int): Double {
         return numericalDiff(f, x.toDouble())
+    }
+
+    fun numericalGradient(f: (Double) -> Double, x: Matrix): Matrix {
+        return x.apply { numericalDiff(f, it) }
     }
 }
