cc8dbdddb57527edfb65bf4abf8881312635d071
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-08-15 17:36:31 +0900

    last modification

--- src/main/kotlin/com/improve_future/dlk/TwoLayerNet.kt
+++ src/main/kotlin/com/improve_future/dlk/TwoLayerNet.kt
@@ -1,15 +1,37 @@
 package com.improve_future.dlk
 
-/**
- * Created by kenji on 17/09/28.
- */
+import com.improve_future.dlk.layer.BatchAffineLayer
+
 class TwoLayerNet(
         inputSize: Int,
         hiddenSize: Int,
         outputSize: Int,
         weightInitStd: Double = 0.01
 ) {
+    val params = mapOf(
+            "w1" to
+                    weight*
+    )
+    val layers = mapOf {
+        "Affine1" to BatchAffineLayer
+    }
     init {
 
+    }
+
+    fun predict(x: Matrix) {
+        return x
+    }
+
+    fun loss(x: Matrix, t: Matrix) {
+        val y = predict(x)
+        return lastLayer.forward(y, t)
+    }
+
+    fun accuracy(x: Matrix, t: Matrix) {
+        val y = predict(x)
+        y
+
+        return MatMath.sum()
     }
 }
